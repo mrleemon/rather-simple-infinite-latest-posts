6a1d490931b639607458f9117d8a943e33609652
leemon <tacrapo@gmail.com>
2021-03-06 22:59:20 +0100

    Store posts offset in sessionStorage when changing pages

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,5 +1,15 @@
 ( function( $ ) {
 
+    // Remember offset when changing pages
+    let button = document.querySelector( '.widget_infinite_latest_posts .load-more' );
+    let offset = sessionStorage.getItem( 'posts-offset' );
+    if ( offset !== null ) {
+        button.dataset.offset = offset;
+    }
+    window.addEventListener( 'beforeunload', () => {
+        sessionStorage.setItem( 'posts-offset', button.dataset.offset );
+    });
+    
 	$( function() {
 		
         $( '.widget_infinite_latest_posts .load-more' ).on( 'click',  function( e ) {
@@ -15,7 +25,8 @@
                     success: function( result ) {
                         /* handling of the output returned by PHP function */
                         link = $( '.widget_infinite_latest_posts .infinite-posts' ).append( result );
-                        $this.data( 'offset', $this.data( 'offset' ) + 5 );
+                        $this.attr( 'data-offset', parseInt( $this.attr( 'data-offset' ) ) + 5 );
+                        /* $this.data( 'offset', $this.data( 'offset' ) + 5 ); */
                     },
                     error: function() {
                         /* what to do if there's a server error, like 404 */
