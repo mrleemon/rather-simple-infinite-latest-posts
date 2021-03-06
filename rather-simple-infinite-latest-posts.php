506942b96217756638f73023e39002f24c96811f
leemon <tacrapo@gmail.com>
2021-03-06 16:36:07 +0100

    Add code files Add readme.txt and license.txt Add .gitattributes and .gitignore

---
+++ rather-simple-infinite-latest-posts.php
@@ -0,0 +1,194 @@
+<?php
+/*
+Plugin Name: Rather Simple Infinite Latest Posts
+Plugin URI:
+Description: A really simple infinite latest posts widget.
+Version: 1.0
+Author: Oscar Ciutat
+Author URI: http://oscarciutat.com/code/
+Text Domain: rsilp
+License: GPLv2 or later
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class Rather_Simple_Infinite_Latest_Posts extends WP_Widget {
+    
+	/**
+	 * Sets up a new Infinite Latest Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 */
+	public function __construct() {
+		$widget_ops = array(
+			'classname'                   => 'widget_infinite_latest_posts',
+			'description'                 => __( 'Your site&#8217;s most recent Posts.' ),
+			'customize_selective_refresh' => true,
+		);
+		parent::__construct( 'infinite-latest-posts', __( 'Infinite Latest Posts', 'rsilp' ), $widget_ops );
+		$this->alt_option_name = 'widget_infinite_latest_posts';
+	}
+
+	/**
+	 * Outputs the content for the current Recent Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param array $instance Settings for the current Recent Posts widget instance.
+	 */
+	public function widget( $args, $instance ) {
+		if ( ! isset( $args['widget_id'] ) ) {
+			$args['widget_id'] = $this->id;
+		}
+
+		$default_title = __( 'Infinite Latest Posts', 'rsilp' );
+		$title         = ( ! empty( $instance['title'] ) ) ? $instance['title'] : $default_title;
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+		if ( ! $number ) {
+			$number = 5;
+		}
+		$show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+
+		$r = new WP_Query(
+			/**
+			 * Filters the arguments for the Recent Posts widget.
+			 *
+			 * @since 3.4.0
+			 * @since 4.9.0 Added the `$instance` parameter.
+			 *
+			 * @see WP_Query::get_posts()
+			 *
+			 * @param array $args     An array of arguments used to retrieve the recent posts.
+			 * @param array $instance Array of settings for the current widget.
+			 */
+			apply_filters(
+				'widget_posts_args',
+				array(
+					'posts_per_page'      => $number,
+					'no_found_rows'       => true,
+					'post_status'         => 'publish',
+					'ignore_sticky_posts' => true,
+				),
+				$instance
+			)
+		);
+
+		if ( ! $r->have_posts() ) {
+			return;
+		}
+		?>
+
+		<?php echo $args['before_widget']; ?>
+
+		<?php
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+
+		$format = current_theme_supports( 'html5', 'navigation-widgets' ) ? 'html5' : 'xhtml';
+
+		/** This filter is documented in wp-includes/widgets/class-wp-nav-menu-widget.php */
+		$format = apply_filters( 'navigation_widgets_format', $format );
+
+		if ( 'html5' === $format ) {
+			// The title may be filtered: Strip out HTML and make sure the aria-label is never empty.
+			$title      = trim( strip_tags( $title ) );
+			$aria_label = $title ? $title : $default_title;
+			echo '<nav role="navigation" aria-label="' . esc_attr( $aria_label ) . '">';
+		}
+		?>
+
+		<ul>
+			<?php foreach ( $r->posts as $recent_post ) : ?>
+				<?php
+				$post_title   = get_the_title( $recent_post->ID );
+				$title        = ( ! empty( $post_title ) ) ? $post_title : __( '(no title)' );
+				$aria_current = '';
+
+				if ( get_queried_object_id() === $recent_post->ID ) {
+					$aria_current = ' aria-current="page"';
+				}
+				?>
+				<li>
+					<a href="<?php the_permalink( $recent_post->ID ); ?>"<?php echo $aria_current; ?>><?php echo $title; ?></a>
+					<?php if ( $show_date ) : ?>
+						<span class="post-date"><?php echo get_the_date( '', $recent_post->ID ); ?></span>
+					<?php endif; ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+
+		<?php
+		if ( 'html5' === $format ) {
+			echo '</nav>';
+		}
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Handles updating the settings for the current Recent Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via
+	 *                            WP_Widget::form().
+	 * @param array $old_instance Old settings for this instance.
+	 * @return array Updated settings to save.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance              = $old_instance;
+		$instance['title']     = sanitize_text_field( $new_instance['title'] );
+		$instance['number']    = (int) $new_instance['number'];
+		$instance['show_date'] = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+		return $instance;
+	}
+
+	/**
+	 * Outputs the settings form for the Recent Posts widget.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $instance Current settings.
+	 */
+	public function form( $instance ) {
+		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+		$show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+			<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" />
+		</p>
+
+		<p>
+			<input class="checkbox" type="checkbox"<?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label>
+		</p>
+		<?php
+	}
+}
+
+add_action( 'widgets_init', function() { return register_widget( 'Rather_Simple_Infinite_Latest_Posts' ); } );
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Rather Simple Infinite Latest Posts ===
+Contributors: leemon
+Tags: posts, widget
+Requires at least: 4.9
+Tested up to: 5.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A really simple infinite latest posts widget.
+
+== Description ==
+
+A really simple infinite latest posts widget.
+
+This plugin is experimental and is provided with no support or warranty. 
+
+== Installation ==
+1. Upload the extracted plugin folder and contained files to your /wp-content/plugins/ directory
+2. Activate the plugin through the "Plugins" menu in WordPress
+
+== Changelog ==
+= 1.0 =
+* Initial release
