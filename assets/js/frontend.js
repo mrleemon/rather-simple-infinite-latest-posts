8e58522387bba10c6039f994b11766eea113e850
leemon <tacrapo@gmail.com>
2021-03-08 15:38:16 +0100

    Improve JS code

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,28 +1,23 @@
 ( function( $ ) {
 
-    // Remember offset when changing pages
-    let button = document.querySelector( '.widget_infinite_latest_posts .load-more' );
-    let offset = sessionStorage.getItem( 'posts-offset' );
+    var offset = sessionStorage.getItem( 'posts-offset' );
+    var button = document.querySelector( '.widget_infinite_latest_posts .load-more' );
     if ( offset !== null ) {
         button.dataset.offset = offset;
     }
-    let div = document.querySelector( '.widget_infinite_latest_posts .infinite-posts' );
-    /*
-    let html = sessionStorage.getItem( 'posts-html' );
-    if ( html !== null ) {
-        div.insertAdjacentHTML( 'afterend', html );
-    }
-    */
+    var div = document.querySelector( '.widget_infinite_latest_posts .infinite-posts' );
     window.addEventListener( 'beforeunload', () => {
+        // Store posts offset in sessionStorage when changing pages
         sessionStorage.setItem( 'posts-offset', button.dataset.offset );
+        // Store posts markup in sessionStorage when changing pages
         sessionStorage.setItem( 'posts-html', div.innerHTML );
     });
     
 	$( function() {
 		
-        let sidebar = document.querySelector( '.sidebar' );
+        var sidebar = document.querySelector( '.sidebar' );
 
-        let html = sessionStorage.getItem( 'posts-html' );
+        var html = sessionStorage.getItem( 'posts-html' );
         if ( html == null ) {
             // Get posts from the database if sessionStorage is empty
             $this = $( '.widget_infinite_latest_posts .load-more' );
@@ -48,15 +43,15 @@
         } else {
             // Get posts from sessionStorage on subsequent loads
             $( '.widget_infinite_latest_posts .infinite-posts' ).html( html );
-            let top = sessionStorage.getItem( 'sidebar-scroll' );
-            console.log('scroll-afterload:'+top);
+            // Get stored sidebar scroll position and move to it
+            var top = sessionStorage.getItem( 'sidebar-scroll' );
             if ( top !== null ) {
                 sidebar.scrollTop = parseInt( top, 10 );
             }
         }
 
         window.addEventListener( 'beforeunload', () => {
-            console.log('scroll-beforeunload:'+sidebar.scrollTop);
+            // Store sidebar scroll position in sessionStorage when changing pages
             sessionStorage.setItem( 'sidebar-scroll', sidebar.scrollTop );
         });
 
@@ -74,7 +69,6 @@
                         /* handling of the output returned by PHP function */
                         $( '.widget_infinite_latest_posts .infinite-posts' ).append( result );
                         $this.attr( 'data-offset', parseInt( $this.attr( 'data-offset' ) ) + 2 );
-                        /* $this.data( 'offset', $this.data( 'offset' ) + 5 ); */
                     },
                     error: function() {
                         /* what to do if there's a server error, like 404 */
