c7e0e03e04d829d276d1bc4f41b2c609ee3e9e4e
leemon <tacrapo@gmail.com>
2021-04-21 23:54:03 +0200

    Replace AJAX callback functions with done() and fail() promises

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -19,54 +19,50 @@
         sessionStorage.setItem( 'posts-html', div.innerHTML );
     });
     
-	$( function() {
+    $( function() {
 		
         var sidebar = document.querySelector( '.sidebar' );
 
         $this = $( '.widget_infinite_latest_posts .load-more' );
-        $.ajax( rsilp_params_rest.rest_url + 'rsilp/v1/posts/',
-            {
-                method: 'GET',
-                cache: true,
-                beforeSend: function ( xhr ) {
-                    xhr.setRequestHeader( 'X-WP-Nonce', rsilp_params_rest.rest_nonce )
-                },
-                data: {
-                    number: $this.attr( 'data-number' ),
-                    offset: 0,
-                    total: $this.attr( 'data-total' ),
-                },
-                success: function( result ) {
-                    var html = '';
-                    var posts = result.posts;
-                    var numposts = result.numposts;
-                    for ( var i = 0; i < posts.length; i++ ) {
-                        var post = posts[i];
-                        html += '<article id="post-' + post.ID + '" class="' + post.post_class.join(' ') + '">';
-                        html += '<header class="entry-header">';
-                        html += '<h2 class="entry-title">' + post.post_title + '</h2>';
-                        html += '</header>';
-                        html += '<div class="entry-content">' + post.post_content + '</div>';
-                        html += '<div class="entry-footer">' + post.post_edit_link + '</div>';
-                        html += '</article>';
-                    }
-                    $( '.widget_infinite_latest_posts .infinite-posts' ).html( html );
-                    // Get stored sidebar scroll position and move to it
-                    var top = sessionStorage.getItem( 'sidebar-scroll' );
-                    if ( top !== null ) {
-                        sidebar.scrollTop = parseInt( top, 10 );
-                    }
-                    var total = parseInt( $this.attr( 'data-total' ) );
-                    if ( total <= numposts ) {
-                        // Show button if there are more posts to load
-                        $this.show();
-                    }
-                },
-                error: function() {
-                    /* what to do if there's a server error, like 404 */
-                }
+        $.ajax( rsilp_params_rest.rest_url + 'rsilp/v1/posts/', {
+            method: 'GET',
+            cache: true,
+            beforeSend: function ( xhr ) {
+                xhr.setRequestHeader( 'X-WP-Nonce', rsilp_params_rest.rest_nonce )
+            },
+            data: {
+                number: $this.attr( 'data-number' ),
+                offset: 0,
+                total: $this.attr( 'data-total' ),
             }
-        );
+        }).done( function( result ) {                
+            var html = '';
+            var posts = result.posts;
+            var numposts = result.numposts;
+            for ( var i = 0; i < posts.length; i++ ) {
+                var post = posts[i];
+                html += '<article id="post-' + post.ID + '" class="' + post.post_class.join(' ') + '">';
+                html += '<header class="entry-header">';
+                html += '<h2 class="entry-title">' + post.post_title + '</h2>';
+                html += '</header>';
+                html += '<div class="entry-content">' + post.post_content + '</div>';
+                html += '<div class="entry-footer">' + post.post_edit_link + '</div>';
+                html += '</article>';
+            }
+            $( '.widget_infinite_latest_posts .infinite-posts' ).html( html );
+            // Get stored sidebar scroll position and move to it
+            var top = sessionStorage.getItem( 'sidebar-scroll' );
+            if ( top !== null ) {
+                sidebar.scrollTop = parseInt( top, 10 );
+            }
+            var total = parseInt( $this.attr( 'data-total' ) );
+            if ( total <= numposts ) {
+                // Show button if there are more posts to load
+                $this.show();
+            }
+        }).fail( function() {
+            /* what to do if there's a server error, like 404 */
+        });
 
         window.addEventListener( 'beforeunload', () => {
             // Store sidebar scroll position in sessionStorage when changing pages
@@ -76,46 +72,42 @@
         $( '.widget_infinite_latest_posts .load-more' ).on( 'click',  function( e ) {
             e.preventDefault();
             $this = $( this );
-            $.ajax( rsilp_params_rest.rest_url + 'rsilp/v1/posts/',
-                {
-                    method: 'GET',
-                    cache: true,
-                    beforeSend: function ( xhr ) {
-                        xhr.setRequestHeader( 'X-WP-Nounce', rsilp_params_rest.rest_nonce )
-                    },
-                    data: {
-                        number: $this.attr( 'data-number' ),
-                        offset: $this.attr( 'data-offset' ),
-                        total: $this.attr( 'data-number' ),
-                    },
-                    success: function( result ) {
-                        var html = '';
-                        var posts = result.posts;
-                        var numposts = result.numposts;
-                        for ( var i = 0; i < posts.length; i++ ) {
-                            var post = posts[i];
-                            html += '<article id="post-' + post.ID + '" class="' + post.post_class.join(' ') + '">';
-                            html += '<header class="entry-header">';
-                            html += '<h2 class="entry-title">' + post.post_title + '</h2>';
-                            html += '</header>';
-                            html += '<div class="entry-content">' + post.post_content + '</div>';
-                            html += '<div class="entry-footer">' + post.post_edit_link + '</div>';
-                            html += '</article>';
-                        }
-                        $( '.widget_infinite_latest_posts .infinite-posts' ).append( html );
-                        $this.attr( 'data-offset', parseInt( $this.attr( 'data-offset' ) ) + parseInt( $this.attr( 'data-number' ) ) );
-                        var total = parseInt( $this.attr( 'data-total' ) ) + parseInt( $this.attr( 'data-number' ) );
-                        $this.attr( 'data-total', total );
-                        if ( total >= numposts ) {
-                            // Hide button if there are no more posts to load
-                            $this.hide();
-                        }
-                    },
-                    error: function() {
-                        /* what to do if there's a server error, like 404 */
-                    }
+            $.ajax( rsilp_params_rest.rest_url + 'rsilp/v1/posts/', {
+                method: 'GET',
+                cache: true,
+                beforeSend: function ( xhr ) {
+                    xhr.setRequestHeader( 'X-WP-Nounce', rsilp_params_rest.rest_nonce )
+                },
+                data: {
+                    number: $this.attr( 'data-number' ),
+                    offset: $this.attr( 'data-offset' ),
+                    total: $this.attr( 'data-number' ),
                 }
-            );
+            }).done( function( result ) {                   
+                var html = '';
+                var posts = result.posts;
+                var numposts = result.numposts;
+                for ( var i = 0; i < posts.length; i++ ) {
+                    var post = posts[i];
+                    html += '<article id="post-' + post.ID + '" class="' + post.post_class.join(' ') + '">';
+                    html += '<header class="entry-header">';
+                    html += '<h2 class="entry-title">' + post.post_title + '</h2>';
+                    html += '</header>';
+                    html += '<div class="entry-content">' + post.post_content + '</div>';
+                    html += '<div class="entry-footer">' + post.post_edit_link + '</div>';
+                    html += '</article>';
+                }
+                $( '.widget_infinite_latest_posts .infinite-posts' ).append( html );
+                $this.attr( 'data-offset', parseInt( $this.attr( 'data-offset' ) ) + parseInt( $this.attr( 'data-number' ) ) );
+                var total = parseInt( $this.attr( 'data-total' ) ) + parseInt( $this.attr( 'data-number' ) );
+                $this.attr( 'data-total', total );
+                if ( total >= numposts ) {
+                    // Hide button if there are no more posts to load
+                    $this.hide();
+                }
+            }).fail( function() {
+                /* what to do if there's a server error, like 404 */
+            });
         });
 
 	});
