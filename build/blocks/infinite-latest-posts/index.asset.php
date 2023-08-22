394bcc1bb1295e2028162b49e9074e5fab3cd236
leemon <tacrapo@gmail.com>
2023-08-23 01:20:32 +0200

    Update apiVersion to 3

--- build/blocks/infinite-latest-posts/index.asset.php
+++ build/blocks/infinite-latest-posts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'e39cd15f2b9dd964e994');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '89d14cf3701886ec203a');
