02e839b1a46fe9c9a751ea332206a13a4f352581
leemon <tacrapo@gmail.com>
2024-05-28 10:46:26 +0200

    Rebuild

--- build/blocks/infinite-latest-posts/index.asset.php
+++ build/blocks/infinite-latest-posts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '89d14cf3701886ec203a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'f3093c90ca440fb24ed0');
