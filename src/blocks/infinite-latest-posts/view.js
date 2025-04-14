730b85e4463cd44a353f9bc2da09274c67d74990
leemon <tacrapo@gmail.com>
2025-04-14 13:13:16 +0200

    Remove spaces

--- src/blocks/infinite-latest-posts/view.js
+++ src/blocks/infinite-latest-posts/view.js
@@ -9,29 +9,29 @@ store('rsilp-store', {
 			if (context?.posts) {
 				// Create fragment.
 				const fragment = document.createDocumentFragment();
-			
+
 				context.posts.forEach(post => {
 					const article = document.createElement('article');
 					article.id = `post-${post.id}`;
 					article.className = 'wp-block-occ-rather-simple-infinite-latest-posts__post';
-			
+
 					const title = document.createElement('h2');
 					title.className = 'wp-block-occ-rather-simple-infinite-latest-posts__post-title';
 					title.innerHTML = post.title.rendered;
-			
+
 					const content = document.createElement('div');
 					content.className = 'wp-block-occ-rather-simple-infinite-latest-posts__post-content';
 					content.innerHTML = post.content.rendered;
-			
+
 					article.appendChild(title);
 					article.appendChild(content);
-			
+
 					// Add article to the fragment.
 					fragment.appendChild(article);
 				});
-			
+
 				// Add fragment to the DOM.
-				element.ref.appendChild(fragment); 
+				element.ref.appendChild(fragment);
 
 				// Dispatch custom event after inserting new posts
 				// for external scripts to hook into.
