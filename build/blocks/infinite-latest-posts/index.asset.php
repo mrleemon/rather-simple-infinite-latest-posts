b52869d39d4a3704e3f1bc0d632db083740d3e3b
leemon <tacrapo@gmail.com>
2025-04-15 13:59:04 +0200

    Fix import

--- build/blocks/infinite-latest-posts/index.asset.php
+++ build/blocks/infinite-latest-posts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'f8b1c2e8ae28815dbd71');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'bded96c938e9eaed5a4e');
