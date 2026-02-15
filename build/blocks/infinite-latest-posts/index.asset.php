62f9fd16ec460e1cfd358092dd63693f07fc6f4d
leemon <tacrapo@gmail.com>
2026-02-15 17:33:35 +0100

    Rebuild

--- build/blocks/infinite-latest-posts/index.asset.php
+++ build/blocks/infinite-latest-posts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'bded96c938e9eaed5a4e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '66a4bb92a7c78b18cd92');
--- build/blocks/infinite-latest-posts/view.asset.php
+++ build/blocks/infinite-latest-posts/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '936efb94b9efb14cff5b', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '9181fd41fcdf63f45465', 'type' => 'module');
